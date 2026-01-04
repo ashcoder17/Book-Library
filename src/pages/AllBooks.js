6630aee65171e946cffbc4d1e4d1199ae0d4fba2
dusk <asutoshpradhan424@gmail.com>
2026-01-05 04:59:02 +0530

    Changes

--- src/pages/AllBooks.js
+++ src/pages/AllBooks.js
@@ -4,8 +4,9 @@ import useFetchBooks from '../hooks/useFetchBooks';
 import PageWrapper from "../components/PageWrapper";
 
 const BookList = ({ books }) => (
-  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-    {books.map(book => <BookCard key={book.id} book={book} />)}
+  <div style={{ display: 'flex', gap: '20px', justifyContent: 'space-evenly' }}>
+    {console.log(books)}
+    {books.map(book => <BookCard key={book.id} id={book.id} title={book.volumeInfo.title} author={book.volumeInfo.authors?.join(', ')} imgsrc={book.volumeInfo.imageLinks.smallThumbnail}/>)}
   </div>
 );
 
@@ -13,12 +14,12 @@ const BookList = ({ books }) => (
 const BookListWithLoading = withLoading(BookList);
 
 const AllBooks = () => {
-  const { data, loading } = useFetchBooks('/books.json');
+  const { data, loading } = useFetchBooks('https://www.googleapis.com/books/v1/volumes?q=harry+potter');
 
   return (
     <PageWrapper>
       <h2>Library Catalog</h2>
-      <BookListWithLoading isLoading={loading} books={data} />
+      <BookListWithLoading isLoading={loading} books={data.items} />
     </PageWrapper>
   );
 };
